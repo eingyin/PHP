333212fe790a403db4bb051ee568712ebcbc3e4b
War War Kyaw <warwar67890@gmail.com>
2023-01-26 10:20:47 +0630

    [Encrypting password using hashing function and soring encrpted password in database]

--- admin/registration-finish.php
+++ admin/registration-finish.php
@@ -3,6 +3,8 @@
  $name = $_POST['name'];
  $password = $_POST['password1'];
 
+ $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+
 $select = mysqli_query($conn, "SELECT * FROM admins WHERE name = '".$_POST['name']."'");
 if(mysqli_num_rows($select)) {
     $message = 'Admin name already exist!.Plase change name.';
@@ -13,10 +15,7 @@ if(mysqli_num_rows($select)) {
     </SCRIPT>";
 }
 else{
-    $sql = "INSERT INTO admins (name, password) VALUES ('$name', '$password')";
+    $sql = "INSERT INTO admins (name, password) VALUES ('$name', '$hashed_password')";
     mysqli_query($conn, $sql);
     header("location: product-list.php");
 }
-?>
- 
-
